74f39754e6cdc3e1dad2f9140025f313e7413843
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-07-23 22:39:36 +0200

    ssh-host-connect.sh

---
+++ tools/ssh-host-connect.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# "grep -P \"^Host ([^*]+)$\" $HOME/.ssh/config | sed 's/Host //'"
+# SSH_HOSTS=$(grep -P "^Host ([^*]+)$" $HOME/.ssh/config | sed 's/Host //')
+SSH_HOSTS=$(grep -P "^Host ([^*]+)$" ~/.ssh/config | sed 's/Host //')
+i=1
+for host in $SSH_HOSTS; do
+  echo "[ ${i} ] - ${host}"
+  SSH_HOST[${i}]="${host}"
+  ((i++))
+done
+echo
+input_sel=0
+while [[ ${input_sel} -lt 1 ||  ${input_sel} -gt ${i} ]]; do
+  read -p " Select a SSH HOST: " input_sel
+done
+SSH_HOST_POINT="${SSH_HOST[${input_sel}]}"
+ssh $SSH_HOST_POINT
