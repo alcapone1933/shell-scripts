e1f6cb8606126f1669ead3e6d9e7aadbfd21e542
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-07-26 22:25:58 +0200

    „install/docker-and-compose-install.sh“ ändern

--- install/docker-and-compose-install.sh
+++ install/docker-and-compose-install.sh
@@ -7,10 +7,10 @@ fi
 apt-get update
 apt-get install -y ca-certificates curl gnupg lsb-release
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 docker -v
