51789b173100a6334bd421a815239b6c070f1d77
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-10-22 19:40:11 +0200

    „install.sh“ ändern

--- install.sh
+++ install.sh
@@ -68,6 +68,73 @@ function Docker_Compose_install() {
         return 0
     fi
 }
+function Docker_and_Compose_remove() {
+    if whiptail --yesno "Docker and Compose Remove" 10 100; then
+        sudo apt-get remove --purge docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+        sudo apt-get autoremove -y
+        rm -vrf /usr/local/bin/docker-compose && rm -vrf /usr/bin/docker-compose
+        clear
+    else
+        clear
+        return 0
+    fi
+}
+function Docker_remove() {
+    if whiptail --yesno "Docker Remove" 10 100; then
+        sudo apt-get remove --purge docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+        sudo apt-get autoremove -y
+        clear
+    else
+        clear
+        return 0
+    fi
+}
+function Docker_Compose_remove() {
+    if whiptail --yesno "Docker Compose Remove" 10 100; then
+        rm -vrf /usr/local/bin/docker-compose && rm -vrf /usr/bin/docker-compose
+        clear
+    else
+        clear
+        return 0
+    fi
+}
+function CLEAN() {
+while [ true ];
+do
+CHOICE=$(
+whiptail --title "DOCKER RESTORE MENU" --menu "Choose an option" 18 100 10 \
+    "[ 1 ]" "Docker and Compose Remove" \
+    "[ 2 ]" "Docker Remove" \
+    "[ 3 ]" "Docker Compose Remove" \
+    "[ R ]" "Return to Start Menu" \
+    "[ E ]" "EXIT"  3>&1 1>&2 2>&3
+)
+    # usage;
+    case $CHOICE in
+        "[ 1 ]")
+            Docker_and_Compose_remove
+            ;;
+        "[ 2 ]")
+            Docker_remove
+            ;;
+        "[ 3 ]")
+            Docker_Compose_remove
+            ;;
+        "[ R ]")
+            return 0
+            ;;
+        "[ E ]")
+            EXIT
+            clear
+            exit 1
+            ;;
+        *)
+            clear
+            exit 1
+            ;;
+    esac
+done
+}
 function EXIT() {
 EXIT=$(
 echo "========================================="
@@ -89,7 +156,8 @@ whiptail --title "DOCKER RESTORE MENU" --menu "Choose an option" 18 100 10 \
     "[ 3 ]" "Docker install on DEBIAN" \
     "[ 4 ]" "Docker install on UBUNTU" \
     "[ 5 ]" "Docker Compose install" \
-    "[ e ]" "exit"  3>&1 1>&2 2>&3
+    "[ 6 ]" "Remove Docker or Docker Compose" \
+    "[ E ]" "EXIT"  3>&1 1>&2 2>&3
 )
     # usage;
     case $CHOICE in
@@ -108,10 +176,10 @@ whiptail --title "DOCKER RESTORE MENU" --menu "Choose an option" 18 100 10 \
         "[ 5 ]")
             Docker_Compose_install
             ;;
-        "[ h ]")
-            HELP
+        "[ 6 ]")
+            CLEAN
             ;;
-        "[ e ]")
+        "[ E ]")
             EXIT
             clear
             exit 1
