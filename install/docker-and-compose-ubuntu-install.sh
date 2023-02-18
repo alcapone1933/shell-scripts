134eb90a9dadf9e669c143bb94d02ad5d9e01534
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2023-02-19 00:23:48 +0100

    update

--- install/docker-and-compose-ubuntu-install.sh
+++ install/docker-and-compose-ubuntu-install.sh
@@ -5,14 +5,14 @@ if [ `whoami` != root ]; then
 fi
 # Docker install
 apt-get update
-apt-get -y install ca-certificates curl gnupg lsb-release
-mkdir -p /etc/apt/keyrings
+apt-get install -y ca-certificates curl gnupg lsb-release
+mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 docker -v 
 
 # Docker Compose install
