a0f00f166aabd38d7d3ccda892f5c18db7d858ca
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2023-02-19 00:00:40 +0100

    update

--- tools/ssh-host-connect-whiptail.sh
+++ tools/ssh-host-connect-whiptail.sh
@@ -3,23 +3,36 @@
 # SSH_HOSTS=$(grep -P "^Host ([^*]+)$" $HOME/.ssh/config | sed 's/Host //')
 SSH_HOSTS=$(grep -P "^Host ([^*]+)$" ~/.ssh/config | sed 's/Host //')
 i=1
+LIST_SSH_HOSTS=$(
 for hosts in $SSH_HOSTS; do
-  echo "[ ${i} ] - ${hosts}"
-  SSH_HOST[${i}]="${hosts}"
-  ((i++))
+    echo "[ ${i} ] - ${hosts}"
+    ((i++))
+done
+)
+for hosts in $SSH_HOSTS; do
+    SSH_HOST[${i}]="${hosts}"
+    ((i++))
 done
 echo
 input_sel=0
 while [[ ${input_sel} -lt 1 ||  ${input_sel} -gt ${i} ]]; do
     echo
     if [ $# -lt 1 ]; then
-        read -p " Select a SSH HOST: " input_sel
+        input_sel=$(whiptail --title "ssh config" --inputbox "$LIST_SSH_HOSTS" 45 110 3>&1 1>&2 2>&3)
         clear
     else
         export input_sel="$1"
         clear
     fi
-    echo
+    # read -p " Select a SSH HOST: " input_sel
+    echo "$input_sel"
+    if [ -z "$input_sel" ]; then
+        clear
+        exit 0
+    else
+        echo "$input_sel"
+        clear
+    fi
 done
 SSH_HOST_POINT="${SSH_HOST[${input_sel}]}"
 ssh $SSH_HOST_POINT
