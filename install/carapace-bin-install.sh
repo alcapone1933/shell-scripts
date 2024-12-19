09c4be9a372d60c7aa1d4b585b6937350bc38cf6
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2024-12-19 23:03:59 +0100

    install/carapace-bin-install.sh aktualisiert

--- install/carapace-bin-install.sh
+++ install/carapace-bin-install.sh
@@ -92,9 +92,9 @@ function update {
     esac
     # Check if an update is available
     echo "Checking for updates to $program_name..."
-    current_version=$($program_cmd --version 2>&1 | sed 's/.* //')
+    # current_version=$($program_cmd --version 2>&1 | sed 's/.* //')
     # current_version=$($program_cmd --version 2>&1 | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-    # current_version=$($program_cmd --version 2>&1 | awk '/[0-9]+\.[0-9]+\.[0-9]+/ {print $NF}')
+    current_version=$($program_cmd --version 2>&1 | awk '/[0-9]+\.[0-9]+\.[0-9]+/ {print $NF}')
     # current_version=$($program_cmd --version 2>&1)
     latest_version=$(curl -s https://api.github.com/repos/${program_repo}/releases/latest | grep 'tag_name' | cut -d\" -f4)
     latest_version_number=$(echo "$latest_version" | sed 's/^v//')
